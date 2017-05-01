9db17e0b56a85da385c440cd1720acb8c49386b7
Vinny Martinez <vinny@vinnymartinez.com>
2017-05-01 09:48:36 -0400

    some test code added

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-const app = "I don't do much.";
+var MyH2 =
+  React.createClass({
+    render() {
+      return React.createElement('h2', {}, 'New Section');
+    }
+  });
+
+var rootElement =
+  React.createElement('div', {},
+    React.createElement('h1', {}, 'hi'),
+    React.createElement('p', {}, 'this is something'),
+    React.createElement('ul', {}, [
+      React.createElement('li', {key: 1}, 'item1'),
+      React.createElement('li', {key: 2}, 'item2'),
+      React.createElement('li', {key: 3}, 'item3'),
+    ]),
+    React.createElement(MyH2, {})
+  );
+
+ReactDOM.render(rootElement, document.getElementById('main'))
